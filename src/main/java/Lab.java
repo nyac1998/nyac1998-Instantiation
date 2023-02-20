c6b3bcef766cc73d537e735ef02c6baca6457570
Nya Currie <nya1998@gmail.com>
2023-02-20 21:36:07 +0000

    instantiate object

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object object = new Object();
+        return new Object();
     }
 }
